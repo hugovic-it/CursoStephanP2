8fa346ec11c8e8a484c5a7995f214911cf102a03
Hugo Victor <hugovic.it@gmail.com>
2023-08-12 11:33:22 -0300

    Utilizando o Flunt para validar

--- src/Domain/Product/Category.cs
+++ src/Domain/Product/Category.cs
@@ -1,6 +1,18 @@
-﻿namespace IWantApp.Domain.Product;
+﻿using Flunt.Validations;
+using System.Diagnostics.Contracts;
+
+namespace IWantApp.Domain.Product;
 public class Category : Entity
 {
     public string Name { get; set; }
-    public bool Active { get; set; } = true;
+    public bool Active { get; set; }
+
+    public Category(string name )
+    {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(name, "Name");
+        AddNotifications(contract);
+        Name = name;
+        Active = true;
+    }
 }
