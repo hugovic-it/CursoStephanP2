dbf1f5985db9b553b01d97eeb2dd5dbaf9b72743
Hugo Victor <hugovic.it@gmail.com>
2023-08-14 11:15:49 -0300

    71. Refatorando o construtor para mais validacoes

--- src/Endpoints/Categories/CategoryPost.cs
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -10,13 +10,8 @@ public class CategoryPost
     public static Delegate Handle => Action;
     public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
     {
-        var category = new Category(categoryRequest.Name)
-        {
-            CreatedBy = "Test",
-            CreatedOn = DateTime.Now,
-            EditedBy = "Test",
-            EditedOn = DateTime.Now
-        };
+        var category = new Category(categoryRequest.Name, "Test", "Test");
+
         if (!category.IsValid)
         {
             return Results.BadRequest(category.Notifications);
