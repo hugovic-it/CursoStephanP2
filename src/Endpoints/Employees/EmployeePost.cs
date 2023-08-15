e449753cd3306d56ec8ace2db0da13c8d3eb3398
Hugo Victor <hugovic.it@gmail.com>
2023-08-15 19:26:53 -0300

    82. Salvando lista de claims

--- src/Endpoints/Employees/EmployeePost.cs
+++ src/Endpoints/Employees/EmployeePost.cs
@@ -5,28 +5,28 @@ using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Employees;
 
-public class EmployeePost{
+public class EmployeePost
+{
     public static string Template => "/employees";
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
     public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager)
     {   //UserName é o "login", o identificador unico desse usuário
-        var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email};
-        var result = userManager.CreateAsync(user,employeeRequest.Password).Result;
-        if (!result.Succeeded){
+        var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
+        var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
+        if (!result.Succeeded)
+        { //verificação 
             return Results.BadRequest(result.Errors.First());
         }
-        //var resultadoDoClaim = contexto.Adicionar(usuárioDesejado,NovoClaim(chave/tipo,valor))
-        var claimResult = 
-            userManager.AddClaimAsync(user, new Claim("EmployeeCode",employeeRequest.EmployeeCode)).Result;
-        if (!claimResult.Succeeded)
-        {
-            return Results.BadRequest(claimResult.Errors.First());
-        }
 
-        //agora, o claim do nome
-        claimResult = 
-            userManager.AddClaimAsync(user, new Claim("Name", employeeRequest.Name)).Result;
+        var userClaims = new List<Claim>
+        {
+        new Claim("EmployeeCode",employeeRequest.EmployeeCode),
+        new Claim("Name", employeeRequest.Name)
+        };
+
+        var claimResult =
+            userManager.AddClaimsAsync(user, userClaims).Result;
         if (!claimResult.Succeeded)
         {
             return Results.BadRequest(claimResult.Errors.First());
